fc5ee8b06319b6da7bc6c236830b84a0e816bd8e
ANTMOD46 <Juntarisil55@gmail.com>
2024-12-24 17:35:46 +0700

    mods

--- gradioapp/views.py
+++ gradioapp/views.py
@@ -24,8 +24,10 @@ with gr.Blocks() as demo:
     gr.ScatterPlot(df, x="weight", y="height",title="ScatterPlot")
 
     gr.ScatterPlot(df, x="weight", y="height", color="ethnicity",title="Breaking out Series by Color")
+    
+    gr.BarPlot(df, x="weight", y="height", x_bin=10, y_aggregate="sum",title="Bar plot")
 
-    # gr.BarPlot(df, x="weight", y="height", x_bin=10, y_aggregate="sum",title="Bar plot")
+     
     
 demo.launch(share=True)
 
