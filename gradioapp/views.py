50dad7ac4f664f03b22bd761c2ea22d83ec1633a
paknorrapat <norrapat.pa.65@ubu.ac.th>
2024-12-24 16:30:33 +0700

    Pak

--- gradioapp/views.py
+++ gradioapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+import gradio as gr
+import pandas as pd
+import numpy as np
+import random
+
+df = pd.DataFrame({
+    'height': np.random.randint(50, 70, 25),
+    'weight': np.random.randint(120, 320, 25),
+    'age': np.random.randint(18, 65, 25),
+    'ethnicity': [random.choice(["white", "black", "asian"]) for _ in range(25)]
+})
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("### Winkzap ")
+    with gr.Row():
+        gr.Label(len(df), label="Heal Count")
+    gr.DataFrame(df)
+
+    # gr.LinePlot(df, x="weight", y="height",title="Weight vs Height")
+    
+demo.launch(share=True)
+
